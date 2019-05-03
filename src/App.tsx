a1318fbddcb32ac75c6fcb83ddeddbda735191c6
Sanjay Kannan <whaatt@gmail.com>
2019-05-03 16:03:17 -0700

    lol

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import "./App.css";
-
-import React from "react";
-import logo from "./logo.svg";
+import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import React from 'react';
+import Row from 'react-bootstrap/Row';
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <Container>
+    <Row>
+      <Col xs={{ span: 1 }}>
+        <h1 className="title">
+          <Badge>Vest</Badge>
+        </h1>
+      </Col>
+      <Col className="text-right my-auto">
+        <h4 className="menu">
+          <Badge>About</Badge>·<Badge>Contact</Badge>·<Badge>GitHub</Badge>
+        </h4>
+      </Col>
+    </Row>
+    <Row className="top-buffer">
+      <Col xs={{ span: 8 }}>
+        <Jumbotron>
+          <h1>
+            <b>Staking With Leverage</b>
+          </h1>
+          <h3>
+            <span className="chevron">>></span> Earn dividends from the largest
+            miners.
+          </h3>
+          <p>
+            <br />
+            Vest lets you purchase the rights to Proof-of-Stake (PoS) mining
+            rewards, allowing you to capture staking profits{' '}
+            <b>without locking up your personal funds</b>.
+          </p>
+          <p>
+            By partnering with only the most reputable stakers, we offer you
+            exceptional reliability and best-in-the-industry profit margins.
+          </p>
+        </Jumbotron>
+      </Col>
+      <Col xs={{ span: 4 }}>
+        <Card className="card-signup">
+          <Card.Body>
+            <Card.Title>
+              <b>Join our alpha!</b>
+            </Card.Title>
+            <Card.Text>
+              We're looking for users to purchase our initial run of staking
+              contracts, which are backed by Polychain Capital.
+              <br />
+              <br />
+              We're reviewing applications on a rolling basis, so take a brief
+              moment to tell us about your staking needs:
+            </Card.Text>
+            <Button variant="primary">Apply Now</Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
-import "./index.css";
+import './index.css';
+import './custom.scss';
 
-import App from "./App";
-import React from "react";
-import ReactDOM from "react-dom";
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
