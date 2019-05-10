b1f47944c171278c601a705f5ff1e957e05d67a6
Sanjay Kannan <whaatt@gmail.com>
2019-05-09 18:38:50 -0700

    temporary shit basename

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -15,30 +15,30 @@ import Splash from './Splash';
 
 const App: React.FC = () => {
   return (
-    <Router>
+    <Router basename="/vest">
       <Container className="top-buffer bottom-buffer">
         <Row>
           <Col>
             <h1 id="title">
-              <Link to="/">
+              <Link to="">
                 <Badge>Vest</Badge>
               </Link>
             </h1>
           </Col>
           <Col xs={{ span: 8 }} className="text-right my-auto">
             <h4 id="menu">
-              <NavLink to="/" exact activeClassName="menu-active">
+              <NavLink to="" exact activeClassName="menu-active">
                 <Badge>Home</Badge>
               </NavLink>
               ·
-              <NavLink to="/about" exact activeClassName="menu-active">
+              <NavLink to="about" exact activeClassName="menu-active">
                 <Badge>About</Badge>
               </NavLink>
             </h4>
           </Col>
         </Row>
         <Route path="/" exact component={Splash} />
-        <Route path="/about" exact component={About} />
+        <Route path="/about" component={About} />
         <Row id="footer">
           <Col>
             ©2019{' '}
